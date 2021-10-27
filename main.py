20d6ee4b129c3e769db9348d014d650404b28228
Alex <24409942+inckie@users.noreply.github.com>
2021-10-27 16:07:50 -0400

    * all base works

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import datetime
+import json
 import random
 import string
-from typing import Optional, List, Dict
-import os
+from io import StringIO
+from typing import Optional, List
+
 import jwt as jwt
 import uvicorn
+from bson import ObjectId
 from fastapi import FastAPI, Depends, Request
 from fastapi import Header, HTTPException, status
+from fastapi.responses import HTMLResponse
 from jose import JWTError, jwt
 from pymongo import MongoClient
-from fastapi.responses import HTMLResponse
+from starlette.responses import StreamingResponse
 from starlette.templating import Jinja2Templates
 
 app = FastAPI()
@@ -23,11 +27,22 @@ ALGORITHM = "HS256"
 
 templates = Jinja2Templates('templates')
 
-async def get_app_bucket(authorization: Optional[str] = Header(None)):
-    if not authorization:
+
+def _new_pin() -> str:
+    while True:
+        pin = ''.join(random.choices(string.digits, k=5))
+        if db["loggs"].find_one({'pin': pin}):
+            continue
+        expires = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+        db["loggs"].insert_one({'pin': pin, 'expires': expires})
+        return pin
+
+
+async def get_app_bucket(Authorization: Optional[str] = Header(None)):
+    if not Authorization:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Auth required')
 
-    token = authorization.replace('Bearer ', '')
+    token = Authorization.replace('Bearer ', '')
 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -47,20 +62,14 @@ async def get_app_bucket(authorization: Optional[str] = Header(None)):
 
 @app.post("/create")
 def create_pin() -> str:
-    while True:
-        pin = ''.join(random.choices(string.digits, k=5))
-        if db["loggs"].find_one({'pin': pin}):
-            continue
-        expires = datetime.datetime.utcnow() + datetime.timedelta(days=1)
-        db["loggs"].insert({'pin': pin, 'expires': expires})
-        return pin
-
+    return _new_pin()
 
 @app.post("/activate")
 def activate(data: dict) -> dict:
     pin = data.get('pin')
     if not pin:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'PIN is required')
+
     appInfo = data.get('appInfo')
     if not appInfo:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'appInfo is required')
@@ -69,35 +78,76 @@ def activate(data: dict) -> dict:
         raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=f'PIN is not found')
 
     # todo: check it its not already activated (does not has appdata)
+    if record.get('appInfo'):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Already taken')
 
-    # todo: insert app data to db
+    objectId = record.get("_id")
+    # insert app data to db
+    # todo: check success
+    db["loggs"].update_one({'_id': objectId}, {'$set': {'appInfo': appInfo}})
 
-    oid = str(record.get("_id"))
-    jwt_token = jwt.encode({"sub": oid}, key=SECRET_KEY, algorithm=ALGORITHM)
+    jwt_token = jwt.encode({"sub": str(objectId)}, key=SECRET_KEY, algorithm=ALGORITHM)
 
     return {
         'jwt': jwt_token,
         'expires': record.get('expires'),
+        'configuration': {
+            'local_logger_url': 'http://192.168.15.237:9080/',
+            'remote_logger_url': 'https://hackatron-logger-2rrbvh5fwa-uc.a.run.app/'
+        }
     }
 
 
 @app.post("/postBatchEvents")
 def post_events(events: List[dict],
                 oid: str = Depends(get_app_bucket)) -> str:
-    # todo: write to db with ObjectId oid
-    pass
+    # todo: add if condition that appInfo is there
+    # todo: check result
+    db["loggs"].update_one({'_id': ObjectId(oid)},
+                           {'$push': {'events': {"$each": events}}})
     return "OK"
+
+
+@app.get("/new_bucket", response_class=HTMLResponse)
+def create_pin(request: Request):
+    pin = _new_pin()
+    return _render_home(request, pin)
+
+
+@app.get("/download/{pin}")
+def download_log(pin: str):
+    record = db["loggs"].find_one({'pin': pin})
+    if not record:
+        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=f'PIN is not found')
+    stream = StringIO()
+    events = record.get('events')
+    # todo: super ineffective
+    stream.write(json.dumps(events))
+    response = StreamingResponse(
+        iter([stream.getvalue()]),
+        media_type='text/json',
+        headers={"Content-Disposition": "inline; filename=log.json"})
+    return response
 
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
+    return _render_home(request, None)
+
+
+def _render_home(request, pin: Optional[str]):
+    entries = list(db["loggs"].find())
+    for entry in entries:
+        entry.pop("_id")
+        entry['has_events'] = bool(entry.pop("events", None))
     return templates.TemplateResponse(
-        "index.tpl", dict(request=request, title="Zapp Logs")
+        "index.tpl", dict(request=request, title="Zapp Logs", entries=entries, pin=pin)
     )
 
-# uvicorn.run(app,
-#             host='0.0.0.0',
-#             port=8000)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+uvicorn.run(app,
+            host='0.0.0.0',
+            port=8000)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,30 +1,59 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="/static/css/styles.css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="/static/css/styles.css">
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <title>{{ title }}</title>
-</head>
-<body>
-<header id="header">
-    <p>Zapp Logs</p>
-</header>
-<main id="content"></main>
-<footer id="footer">Applicaster (c) 2021</footer>
-</body>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <title>{{ title }}</title>
+    </head>
+    <body>
+        <header id="header">
+            <h1>Zapp Logs</h1>
+        </header>
+        <main id="content">
+            {% if pin %}
+                <h2>New PIN created: {{ pin }}</h2>
+            {% endif %}
+
+            <a href="/new_bucket" target="_self">
+                <button type="button" class="btn btn-primary">New bucket</button>
+            </a>
+
+            {%  for entry in entries %}
+            <div class="card" style="width: 36rem; margin: 16px">
+              <div class="card-header d-flex justify-content-between">
+                <b>{{ entry.pin }}</b>
+                <div><b>Expires:</b> {{ entry.expires.strftime('%Y-%m-%d %H:%m') }}</div>
+               </div>
+
+              {% if entry.appInfo %}
+                <div class="card-body d-flex justify-content-between">
+                   <p>{{ entry.appInfo.appName }} {{ entry.appInfo.versionId }}</p>
+                   {% if entry.has_events %}
+                    <a href="download/{{entry.pin}}" target="_blank">
+                      <button type="button" class="btn btn-primary">Download</button>
+                    </a>
+                  {% endif %}
+                </div>
+              {% endif %}
+
+            </div>
+            {% endfor %}
+        </main>
+        <footer id="footer">Applicaster (c) 2021</footer>
+    </body>
 </html>
