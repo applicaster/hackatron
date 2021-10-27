465014fac5cedc5dc7dd617e73380e554e8173ff
Asim Salimli <salimli.asim@gmail.com>
2021-10-27 19:55:46 -0400

    reverted app settings to be able to start app in container

--- main.py
+++ main.py
@@ -184,9 +184,9 @@ def _render_home(request, pin: Optional[str]):
     )
 
 
-uvicorn.run(app,
-            host='0.0.0.0',
-            port=8000)
+# uvicorn.run(app,
+#             host='0.0.0.0',
+#             port=8000)
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
