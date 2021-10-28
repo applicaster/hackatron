40a976b2b85ce2b02307583ad9cfc49f0ae2c608
Asim Salimli <salimli.asim@gmail.com>
2021-10-28 09:27:40 -0400

    cleanup

--- main.py
+++ main.py
@@ -185,10 +185,5 @@ def _render_home(request, pin: Optional[str]):
         "index.tpl", dict(request=request, title="Zapp Logs", entries=entries, pin=pin)
     )
 
-
-# uvicorn.run(app,
-#             host='0.0.0.0',
-#             port=8000)
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
